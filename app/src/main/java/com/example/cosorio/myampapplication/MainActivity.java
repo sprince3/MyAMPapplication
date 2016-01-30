e1858f874511dd846e965c86ef8d89eb0cdb12c0
sprince13 <teach2travel@gmail.com>
2016-01-30 11:59:19 -0700

    changed color

--- app/src/main/java/com/example/cosorio/myampapplication/MainActivity.java
+++ app/src/main/java/com/example/cosorio/myampapplication/MainActivity.java
@@ -8,8 +8,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    static final String console_text = "AMPconsole_string";
+
+    private SeekBar textSize = null;
+    private SeekBar second = null;
+    private TextView textToGrow =null;
+    private RatingBar rate =null;
+    private int FiveStars = 5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        Log.i(console_text, "Entered onCreate Function");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,11 +39,19 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        textSize = (SeekBar) findViewById(R.id.seekBarworks;
+        second = (SeekBar) findViewById(R.id.seekBar3;
+        textToGrow = (TextView) findViewById(R.id.textViewGrow);
+        rate = (RatingBar) findViewById(R.id.ratingBar);
+        rate.setMax(FiveStars);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        Log.i(console_text, "Entered onCreateOptionsMenu Function");
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
